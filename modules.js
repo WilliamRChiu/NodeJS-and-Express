89c509401b22e5db0b270f153e3078b1d1cf9da7
WilliamRChiu <williamrchiu18@gmail.com>
2024-08-17 22:13:13 -0400

    first commit

---
+++ modules.js
@@ -0,0 +1,8 @@
+const names = require('./names');
+const sayHi = require('./utils');
+const data = require('./alternative-flavors');
+require('./mind-grenade');
+
+sayHi("susan");
+sayHi(names.harry);
+sayHi(names.peter);
---
+++ names.js
@@ -0,0 +1,7 @@
+//share
+const peter = "peter";
+const harry = "harry";
+//local
+const secret = "SUPER SECRET";
+
+module.exports = {peter, harry};
